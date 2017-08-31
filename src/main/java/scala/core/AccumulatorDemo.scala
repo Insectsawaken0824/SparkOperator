7b6660a672be97fb81376a946e3ce8c72d9ce8e0
zhaotongchen <18630468824@163.com>
2017-08-31 19:45:35 +0800

    完成累加器Demo

--- src/main/java/scala/core/AccumulatorDemo.scala
+++ src/main/java/scala/core/AccumulatorDemo.scala
@@ -1,15 +1,21 @@
-package scala
+package scala.core
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.util.{DoubleAccumulator, LongAccumulator}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
+  * 累加器
   * Created by zhao on 2017/8/16.
   */
 object AccumulatorDemo {
   def main(args: Array[String]): Unit = {
-    val conf: SparkConf = new SparkConf().setMaster("local").setAppName("AccumulatorDemo")
-    val sc: SparkContext = new SparkContext(conf)
-    sc.textFile("src/name")
-
+    val ss: SparkSession = SparkSession.builder().appName("AccumulatorDemo").master("local").getOrCreate()
+    val fileRdd: RDD[String] = ss.sparkContext.textFile("E:\\project\\SparkOperator\\src\\main\\resources/name.txt")
+    val accumulator: LongAccumulator = ss.sparkContext.longAccumulator
+    val count: Long = fileRdd.flatMap(_.split(" ")).map( x => accumulator.add(1)).count()
+    println(count)
+    println(accumulator.value)
   }
 }
