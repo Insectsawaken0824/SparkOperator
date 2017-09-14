0c785b97a289dce9d6cb735163e56a806e5d9395
zhaotongchen <18630468824@163.com>
2017-09-14 09:54:23 +0800

    增加ml列统计示例

---
+++ src/main/java/scala/ml/StatisticsOperator.scala
@@ -0,0 +1,13 @@
+package scala.ml
+
+/**
+  * Created by zhao on 2017/9/7.
+  * 列统计汇总
+  *
+  */
+object StatisticsOperator {
+  def main(args: Array[String]): Unit = {
+    //读取数据,转换成RDD[Vector]类型
+
+  }
+}
