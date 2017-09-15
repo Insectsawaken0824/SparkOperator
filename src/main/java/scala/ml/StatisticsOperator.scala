29fe0afafbd98d71a48e41355044db41516403af
zhao <824718>
2017-09-15 09:42:29 +0800

    修改统计代码

--- src/main/java/scala/ml/StatisticsOperator.scala
+++ src/main/java/scala/ml/StatisticsOperator.scala
@@ -1,5 +1,9 @@
 package scala.ml
 
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
 /**
   * Created by zhao on 2017/9/7.
   * 列统计汇总
@@ -7,7 +11,14 @@ package scala.ml
   */
 object StatisticsOperator {
   def main(args: Array[String]): Unit = {
+    val ss: SparkSession = SparkSession.builder().appName("StatisticsOperator").master("local").getOrCreate()
+    ss.sparkContext.setLogLevel("WARN")
     //读取数据,转换成RDD[Vector]类型
+    val fileRdd: RDD[String] = ss.sparkContext.textFile(StatisticsOperator.getClass.getClassLoader.getResource("sample_stat.txt").getPath)
+    val doubleRdd: RDD[Array[Double]] = fileRdd.map(_.split("@%split!@")).map(_.map(_.toDouble))
+    val vectorRdd: RDD[Vector] = doubleRdd.map(Vectors.dense(_))
+    vectorRdd.foreach(println)
+    ss.stop()
 
   }
 }
